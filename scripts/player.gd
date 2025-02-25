dcc8e5322bff1685681e96fb454c2ff4be5144d3
maddyP <maanwalczak@gmail.com>
2025-02-25 10:56:25 -0500

    Changed movement to follow pointer

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,27 +1,34 @@
-extends Node2D
+extends RigidBody2D
 
 signal eaten(other:Node)
 signal got_eaten(other:Node)
 
 var size: float = 1
 
-@export var move_speed: float = 1
+@export var move_speed: float = 100
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
+func _physics_process(delta: float) -> void:
+	apply_force((get_global_mouse_position() - global_position)* move_speed)
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("move up"):
-		global_position = global_position + delta * Vector2(0,-1) * move_speed
-	if Input.is_action_pressed("move down"):
-		global_position = global_position + delta * Vector2(0,1) * move_speed
-	if Input.is_action_pressed("move left"):
-		global_position = global_position + delta * Vector2(-1,0) * move_speed
-	if Input.is_action_pressed("move right"):
-		global_position = global_position + delta * Vector2(1,0) * move_speed
+	pass
+	## Keyboard movement
+	##if Input.is_action_pressed("move up"):
+	##	global_position = global_position + delta * Vector2(0,-1) * move_speed
+	##if Input.is_action_pressed("move down"):
+	##	global_position = global_position + delta * Vector2(0,1) * move_speed
+	##if Input.is_action_pressed("move left"):
+	##	global_position = global_position + delta * Vector2(-1,0) * move_speed
+	##if Input.is_action_pressed("move right"):
+	##	global_position = global_position + delta * Vector2(1,0) * move_speed
+	
 
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
